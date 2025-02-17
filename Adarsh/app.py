15901277f86145d942b887d6dc2ce8d998690a26
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2025-02-17 23:43:48 +0530

    Update app.py

--- Adarsh/app.py
+++ Adarsh/app.py
@@ -1,10 +1,21 @@
-from flask import Flask
-app = Flask(__name__)
+from flask import Flask, render_template, request, jsonify
+from Adarsh.bot.plugins.generate_token import get_current_token
 
-@app.route('/')
-def hello_world():
-    return 'Bisal_Files'
+app = Flask(__name__, template_folder="template")
 
+@app.route("/")
+def index():
+    return render_template("req.html")
+
+@app.route("/validate", methods=["POST"])
+def validate_token():
+    user_token = request.form.get("token")
+    current_token = get_current_token()
+    
+    if user_token == current_token:
+        return jsonify({"status": "success", "message": "✅ Access Granted!"})
+    else:
+        return jsonify({"status": "error", "message": "❌ Invalid Token. Please try again."})
 
 if __name__ == "__main__":
-    app.run()
+    app.run(debug=True)
