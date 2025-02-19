4d8d5d4d4b7e97abe8c9b9f5cee1c077fb9b638f
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2025-02-19 23:39:42 +0530

    Update app.py

--- Adarsh/app.py
+++ Adarsh/app.py
@@ -1,10 +1,10 @@
-from flask import Flask
-app = Flask(__name__)
+from aiohttp import web
 
-@app.route('/')
-def hello_world():
-    return 'GreyMatters'
+async def hello(request):
+    return web.Response(text="Hello, World!")
 
+app = web.Application()
+app.router.add_get('/', hello)
 
-if __name__ == "__main__":
-    app.run()
+if __name__ == '__main__':
+    web.run_app(app)
