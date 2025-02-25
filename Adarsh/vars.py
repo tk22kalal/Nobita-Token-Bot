cab63ee94b785397a0633eb6bcaf33f3c5df98b7
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2025-02-25 23:47:47 +0530

    Update vars.py

--- Adarsh/vars.py
+++ Adarsh/vars.py
@@ -1,12 +1,9 @@
 import os
+import random
 from os import getenv, environ
 from dotenv import load_dotenv
-import random
-
-
 
 load_dotenv()
-
 
 
 class Var(object):
@@ -25,7 +22,7 @@ class Var(object):
     PORT = int(getenv('PORT', 8080))
     BIND_ADDRESS = str(getenv('WEB_SERVER_BIND_ADDRESS', '0.0.0.0'))
     PING_INTERVAL = int(environ.get("PING_INTERVAL", "1200"))  # 20 minutes
-    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split())  
+    OWNER_ID = set(int(x) for x in os.environ.get("OWNER_ID", "").split() if x.isdigit())
     NO_PORT = bool(getenv('NO_PORT', False))
     APP_NAME = None
     OWNER_USERNAME = str(getenv('OWNER_USERNAME', 'NobiDeveloperr'))
@@ -36,29 +33,26 @@ class Var(object):
     else:
         ON_HEROKU = False
 
-    # Multiple Cloudflare Worker URLs
-    WORKER_DOMAINS = [
-        "stream1.nextpulse.workers.dev",
-        "stream2.nextpulse.workers.dev",
-        "stream3.nextpulse.workers.dev",
-        "stream4.nextpulse.workers.dev"
-    ]
+    # List of available FQDNs
+    _FQDN_LIST = ["stream.nextpulse.workers.dev", "fly.nextpulse.workers.dev"]
 
-    # Select a random worker
     @staticmethod
-    def get_random_worker():
-        return random.choice(Var.WORKER_DOMAINS)
+    def _get_random_fqdn():
+        """Returns a random FQDN each time it's accessed."""
+        return random.choice(Var._FQDN_LIST)
 
-    # Fix: Set FQDN to always return a worker
-    @property
-    def FQDN(self):
-        return self.get_random_worker()
-
-    # Fix: Ensure URL is dynamically generated
-    @property
-    def URL(self):
-        return f"https://{self.FQDN}/"
+    def __getattr__(self, name):
+        """Dynamically return random FQDN when 'FQDN' or 'URL' is accessed."""
+        if name == "FQDN":
+            return self._get_random_fqdn()
+        if name == "URL":
+            return f"https://{self._get_random_fqdn()}/"
+        raise AttributeError(f"'{self.__class__.__name__}' object has no attribute '{name}'")
 
     DATABASE_URL = str(getenv('DATABASE_URL', ''))
     UPDATES_CHANNEL = str(getenv('UPDATES_CHANNEL', None))
-    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split()))
+    BANNED_CHANNELS = list(set(int(x) for x in str(getenv("BANNED_CHANNELS", "")).split() if x.isdigit()))
+
+
+# Instantiate the class so 'Var.FQDN' and 'Var.URL' work globally
+Var = Var()
