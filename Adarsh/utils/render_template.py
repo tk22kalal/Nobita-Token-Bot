5f2510816814a4d42177cbe46568ce1c04c47cb3
tk22kalal <127711733+tk22kalal@users.noreply.github.com>
2025-07-22 11:43:53 +0530

    Update render_template.py

--- Adarsh/utils/render_template.py
+++ Adarsh/utils/render_template.py
@@ -1,20 +1,18 @@
+import jinja2
+import urllib.parse
+import logging
+import aiohttp
+
 from Adarsh.vars import Var
 from Adarsh.bot import StreamBot
 from Adarsh.utils.human_readable import humanbytes
 from Adarsh.utils.file_properties import get_file_ids
 from Adarsh.server.exceptions import InvalidHash
-import urllib.parse
-import aiofiles
-import logging
-import aiohttp
-import jinja2
 
 async def render_page(id, secure_hash, src=None):
-    file = await StreamBot.get_messages(int(Var.BIN_CHANNEL), int(id))
     file_data = await get_file_ids(StreamBot, int(Var.BIN_CHANNEL), int(id))
     if file_data.unique_id[:6] != secure_hash:
         logging.debug(f"link hash: {secure_hash} - {file_data.unique_id[:6]}")
-        logging.debug(f"Invalid hash for message with - ID {id}")
         raise InvalidHash
 
     src = urllib.parse.urljoin(
@@ -24,6 +22,7 @@ async def render_page(id, secure_hash, src=None):
 
     tag = file_data.mime_type.split("/")[0].strip()
     file_size = humanbytes(file_data.file_size)
+
     if tag in ["video", "audio"]:
         template_file = "Adarsh/template/req.html"
     else:
@@ -32,14 +31,14 @@ async def render_page(id, secure_hash, src=None):
             async with s.get(src) as u:
                 file_size = humanbytes(int(u.headers.get("Content-Length")))
 
+    # Read template using Jinja2
     with open(template_file) as f:
         template = jinja2.Template(f.read())
 
-    file_name = file_data.file_name.replace("_", " ")
-
     return template.render(
-        file_name=file_name,
+        file_name=file_data.file_name.replace("_", " "),
         file_url=src,
         file_size=file_size,
-        file_unique_id=file_data.unique_id,
+        tag=tag,
+        file_unique_id=file_data.unique_id
     )
