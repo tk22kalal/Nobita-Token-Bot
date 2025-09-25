2914c560e2fcb12a5afef9cdfe4a2149bbfe9cce
Tarang Kalal <127711733+tk22kalal@users.noreply.github.com>
2025-09-26 01:12:24 +0530

    Update database.py

--- Adarsh/utils/database.py
+++ Adarsh/utils/database.py
@@ -6,10 +6,18 @@ import time
 
 class Database:
     def __init__(self, uri, database_name):
-        self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
-        self.db = self._client[database_name]
-        self.col = self.db.users
-        self.temp_files = self.db.temp_files
+        self.enabled = bool(uri and uri.strip())
+        if self.enabled:
+            self._client = motor.motor_asyncio.AsyncIOMotorClient(uri)
+            self.db = self._client[database_name]
+            self.col = self.db.users
+            self.temp_files = self.db.temp_files
+        else:
+            self._client = None
+            self.db = None
+            self.col = None
+            self.temp_files = None
+            print("Database disabled - no DATABASE_URL provided")
 
     def new_user(self, id):
         return dict(
@@ -18,35 +26,53 @@ class Database:
         )
 
     async def add_user(self, id):
+        if not self.enabled:
+            return
         user = self.new_user(id)
         await self.col.insert_one(user)
         
     async def add_user_pass(self, id, ag_pass):
+        if not self.enabled:
+            return
         await self.add_user(int(id))
         await self.col.update_one({'id': int(id)}, {'$set': {'ag_p': ag_pass}})
     
     async def get_user_pass(self, id):
+        if not self.enabled:
+            return None
         user_pass = await self.col.find_one({'id': int(id)})
         return user_pass.get("ag_p", None) if user_pass else None
     
     async def is_user_exist(self, id):
+        if not self.enabled:
+            return False
         user = await self.col.find_one({'id': int(id)})
         return True if user else False
 
     async def total_users_count(self):
+        if not self.enabled:
+            return 0
         count = await self.col.count_documents({})
         return count
 
     async def get_all_users(self):
+        if not self.enabled:
+            return []
         all_users = self.col.find({})
         return all_users
 
     async def delete_user(self, user_id):
+        if not self.enabled:
+            return
         await self.col.delete_many({'id': int(user_id)})
 
     # Temporary file storage for intermediate page system
     async def store_temp_file(self, message_data):
         """Store temporary file data and return a unique token"""
+        if not self.enabled:
+            # For testing without database, create a simple token
+            return "test_token_" + secrets.token_urlsafe(8)
+        
         token = secrets.token_urlsafe(16)
         temp_data = {
             'token': token,
@@ -65,6 +91,8 @@ class Database:
 
     async def get_temp_file(self, token):
         """Retrieve temporary file data by token"""
+        if not self.enabled:
+            return None
         temp_data = await self.temp_files.find_one({'token': token})
         if temp_data and temp_data.get('expires_at', 0) > time.time():
             return temp_data
@@ -75,9 +103,13 @@ class Database:
 
     async def delete_temp_file(self, token):
         """Delete temporary file data after stream generation"""
+        if not self.enabled:
+            return
         await self.temp_files.delete_one({'token': token})
 
     async def cleanup_expired_temp_files(self):
         """Clean up expired temporary files"""
+        if not self.enabled:
+            return
         current_time = time.time()
         await self.temp_files.delete_many({'expires_at': {'$lt': current_time}})
