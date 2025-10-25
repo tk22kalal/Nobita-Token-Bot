efe4624b6ffcfd8c3b21a8022e91123337d64266
Tarang Kalal <127711733+tk22kalal@users.noreply.github.com>
2025-10-26 00:33:31 +0530

    Update __init__.py

--- Adarsh/server/__init__.py
+++ Adarsh/server/__init__.py
@@ -5,9 +5,6 @@ from .stream_routes import routes
 
 
 async def web_server():
-    # Increase max size and disable read timeout for large file streaming
-    web_app = web.Application(
-        client_max_size=1024**3,  # 1GB max size
-    )
+    web_app = web.Application(client_max_size=30000000)
     web_app.add_routes(routes)
     return web_app
