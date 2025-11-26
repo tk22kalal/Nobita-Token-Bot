17bd23b2cb3625182a2426800ffee30b60637c0f
Tarang Kalal <127711733+tk22kalal@users.noreply.github.com>
2025-11-26 23:17:38 +0530

    Update keepalive.py

--- Adarsh/utils/keepalive.py
+++ Adarsh/utils/keepalive.py
@@ -13,7 +13,7 @@ async def ping_server():
             async with aiohttp.ClientSession(
                 timeout=aiohttp.ClientTimeout(total=10)
             ) as session:
-                async with session.get(Var.URL) as resp:
+                async with session.get(Var.URL_STREAM) as resp:
                     logging.info("Pinged server with response: {}".format(resp.status))
         except TimeoutError:
             logging.warning("Couldn't connect to the site URL..!")
