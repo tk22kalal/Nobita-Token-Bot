3e302e13f0edf37924a2395636fe3d4ed418cf6f
Tarang Kalal <127711733+tk22kalal@users.noreply.github.com>
2025-11-27 15:32:25 +0530

    Update keepalive.py

--- Adarsh/utils/keepalive.py
+++ Adarsh/utils/keepalive.py
@@ -6,14 +6,17 @@ from Adarsh.vars import Var
 
 
 async def ping_server():
+    """Keep-alive ping that uses THIS instance's URL only (domain independent)."""
     sleep_time = Var.PING_INTERVAL
     while True:
         await asyncio.sleep(sleep_time)
         try:
+            # Use get_base_url() for domain independence
+            ping_url = Var.get_base_url()
             async with aiohttp.ClientSession(
                 timeout=aiohttp.ClientTimeout(total=10)
             ) as session:
-                async with session.get(Var.URL_WEB) as resp:
+                async with session.get(ping_url) as resp:
                     logging.info("Pinged server with response: {}".format(resp.status))
         except TimeoutError:
             logging.warning("Couldn't connect to the site URL..!")
